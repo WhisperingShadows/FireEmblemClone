b51b441b41f667dcbd6f520788d3fc0144f985cf
WhisperingShadows <legoguydania@gmail.com>
2020-11-27 23:24:32 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='FireEmblemClone',
+      version='0.1.0',
+      description='A Python Fire Emblem Heroes clone',
+      author='Dania M.',
+      url='https://fireemblemclone.readthedocs.io/',
+      packages=find_packages(include=["FireEmblemClone", "FireEmblemClone.*"])
+      )
